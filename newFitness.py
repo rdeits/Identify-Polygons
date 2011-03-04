8e602cbbe7eb549155d380f36ffe8a58d4a9af6e
rdeits <robin.deits@gmail.com>
2011-03-03 20:29:37 -0500

    Performance improvements.
    The image is still used, but the data from it is handled through a csv
    reader, which would be a reasonable way to expect data from the java to
    be passed. The GA can now acceptably decode sample.png in 1-2 minutes.

--- newFitness.py
+++ newFitness.py
@@ -4,8 +4,9 @@ import numpy as np
 from scipy import stats
 import Image
 import matplotlib.pyplot as plt
+import csv
 
-image = Image.open('sample_med_rot.png')
+image = Image.open('sample.png')
 (width,height) = image.size
 image = image.convert('RGB')
 image_data = np.resize(image.getdata(),(height,width,3))
@@ -13,13 +14,27 @@ image_data = np.resize(image.getdata(),(height,width,3))
 total_value = 0
 x_weight = 0
 y_weight = 0
+# for x in range(width):
+    # for y in range(height):
+        # x_weight += x * image_data[y][x][0]
+        # y_weight += y * image_data[y][x][0]
+        # total_value += image_data[y][x][0]
+
+f = open('data.csv','wb')
+csv_writer = csv.writer(f)
 for x in range(width):
     for y in range(height):
-        x_weight += x * image_data[y][x][0]
-        y_weight += y * image_data[y][x][0]
-        total_value += image_data[y][x][0]
-
-centroid = (x_weight/total_value, y_weight/total_value)
+        if image_data[y][x][0] > 0:
+            csv_writer.writerow([x,y])
+f.close()
+f = open('data.csv','rb')
+csv_reader = csv.reader(open('data.csv','rb'))
+data = [[float(row[0]),float(row[1])] for row in csv_reader] 
+# print data
+x_list = [el[0] for el in data]
+y_list = [el[1] for el in data]
+# print x_list
+centroid = (sum(x_list)/len(x_list), sum(y_list)/len(y_list))
 print "centroid:",centroid
 
 def regression(x,y):
@@ -30,6 +45,8 @@ def regression(x,y):
     # print "residues",residues
     # x = np.array(x)
     # plt.plot(x,m*x+b)
+    # plt.figure()
+    # plt.plot(x,y,'ro')
     if len(residues) == 0:
         return 0, 0, 0
     else:
@@ -49,35 +66,57 @@ def regression(x,y):
 
 def calculate_error(thetas):
     # plt.figure()
-    # plt.clf()
+    # plt.hold(True)
+    # x = range(width)
+    # y = range(height)
+    # X, Y = np.meshgrid(x,y)
+    # plt.contourf(X,Y,image_data[:,:,0])
     error = 0
+    # slopes = []
+    # intercepts = []
+    # corners = []
     for i, t0 in enumerate(thetas):
         t1 = thetas[(i+1)%len(thetas)]
         x_bin = []
         y_bin = []
-        for x in range(width):
-            for y in range(height):
-                if x == centroid[0]:
-                    if y > centroid[1]:
-                        angle = np.pi/2
-                    else:
-                        angle = np.pi*3/2
+        for x,y in data:
+            if x == centroid[0]:
+                if y > centroid[1]:
+                    angle = np.pi/2
                 else:
-                    angle = np.arctan((y-centroid[1])/(x-centroid[0]))
-                if x < centroid[0]:
-                    angle += np.pi
-                angle %= (2*np.pi)
-                # print "(",x,",",y,") at angle",angle
-                if ((t0 < t1) and (angle > t0 and angle < t1)
-                        or ((t0 >= t1) and (angle > t0 or angle < t1))):
-                    if image_data[y][x][0] > 0:
-                        x_bin.append(x)
-                        y_bin.append(y)
-        if len(x_bin) > 1:
-            error += regression(x_bin,y_bin)[2]
+                    angle = np.pi*3/2
+            else:
+                angle = np.arctan((y-centroid[1])/(x-centroid[0]))
+            if x < centroid[0]:
+                angle += np.pi
+            angle %= (2*np.pi)
+            # print "(",x,",",y,") at angle",angle
+            if ((t0 < t1) and (angle > t0 and angle < t1)
+                    or ((t0 >= t1) and (angle > t0 or angle < t1))):
+                x_bin.append(x)
+                y_bin.append(y)
+        if len(x_bin) > 0:
+            m, b, residues = regression(x_bin,y_bin)
+            error += residues
+            # slopes.append(m)
+            # intercepts.append(b)
         # else:
+            # m = b = 0
             # print "no points between",t0,'and',t1
+        # plt.plot([0,width],[b, width*m+b])
     # print "error =",error
+
+    # for i in range(len(slopes)):
+        # m0 = slopes[i]
+        # b0 = intercepts[i]
+        # m1 = slopes[(i+1)%len(slopes)]
+        # b1 = intercepts[(i+1)%len(slopes)]
+        # x = (b1-b0)/(m0-m1)
+        # corners.append([x, m0*x+b0])
+    # for [x,y] in corners:
+        # plt.plot(x,y,'ro')
+    # plt.xlim([0,width])
+    # plt.ylim([0,height])
     # plt.show()
     return error
 
@@ -96,24 +135,22 @@ def plot_estimate(thetas):
         t1 = thetas[(i+1)%len(thetas)]
         x_bin = []
         y_bin = []
-        for x in range(width):
-            for y in range(height):
-                if x == centroid[0]:
-                    if y > centroid[1]:
-                        angle = np.pi/2
-                    else:
-                        angle = np.pi*3/2
+        for x,y in data:
+            if x == centroid[0]:
+                if y > centroid[1]:
+                    angle = np.pi/2
                 else:
-                    angle = np.arctan((y-centroid[1])/(x-centroid[0]))
-                if x < centroid[0]:
-                    angle += np.pi
-                angle %= (2*np.pi)
-                # print "(",x,",",y,") at angle",angle
-                if ((t0 < t1) and (angle > t0 and angle < t1)
-                        or ((t0 >= t1) and (angle > t0 or angle < t1))):
-                    if image_data[y][x][0] > 0:
-                        x_bin.append(x)
-                        y_bin.append(y)
+                    angle = np.pi*3/2
+            else:
+                angle = np.arctan((y-centroid[1])/(x-centroid[0]))
+            if x < centroid[0]:
+                angle += np.pi
+            angle %= (2*np.pi)
+            # print "(",x,",",y,") at angle",angle
+            if ((t0 < t1) and (angle > t0 and angle < t1)
+                    or ((t0 >= t1) and (angle > t0 or angle < t1))):
+                x_bin.append(x)
+                y_bin.append(y)
         if len(x_bin) > 0:
             m, b, residues = regression(x_bin,y_bin)
             error += residues
@@ -121,8 +158,8 @@ def plot_estimate(thetas):
             intercepts.append(b)
         else:
             m = b = 0
-        plt.plot([0,width],[b, width*m+b])
             # print "no points between",t0,'and',t1
+        plt.plot([0,width],[b, width*m+b])
     # print "error =",error
 
     for i in range(len(slopes)):
@@ -142,5 +179,5 @@ def plot_estimate(thetas):
 
 if __name__ == "__main__":
     # calculate_error([np.pi/4,np.pi*3/4,np.pi*5/4,np.pi*7/4])
-    print calculate_error([0,np.pi/2,np.pi,np.pi*3/2])
-    plot_estimate([0,np.pi/2,np.pi,np.pi*3/2])
+    # print calculate_error([0,np.pi/2,np.pi,np.pi*3/2])
+    print plot_estimate([0,np.pi/2,np.pi,np.pi*3/2])
